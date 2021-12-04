b625de14f2f9e2757e58c33a0e45978dafaacab9
abhishekgupta0598 <abhishekgupta0598@gmail.com>
2021-12-04 16:22:19 +0530

    removed error

--- models/user.js
+++ models/user.js
@@ -18,14 +18,11 @@ const userSchema = Schema({
 });
 
 // encrypt the password before storing
-<<<<<<< HEAD
 // comments make code readable
-=======
 // comments is compulsory
->>>>>>> f88c86a101f1a37b92ae874b674daaa2620bd8cc
 userSchema.methods.encryptPassword = (password) => {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
-  console.log('password', password)
+  console.log("password", password);
 };
 
 userSchema.methods.validPassword = function (candidatePassword) {
